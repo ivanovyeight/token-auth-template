a20667f497e2c9a4aa6eef04e0b27740b328f7f5
Yaroslav Ivanov <58941602+ciphertwentyeight@users.noreply.github.com>
2021-02-09 19:35:02 +0200

    Update urls

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . views import *
+
+urlpatterns = [
+    path('', index),
+    path('register/', register),
+    path('login/<str:token>', login),
+    path("logout", logout),
+]
+
